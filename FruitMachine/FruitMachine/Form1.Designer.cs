37392b18499fd5002943e46fc0948b8a7220574c
abdulshafiq0 <43822591+abdulshafiq0@users.noreply.github.com>
2020-02-14 10:58:25 +0000

    hi

---
+++ FruitMachine/FruitMachine/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FruitMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.btnCashOut = new System.Windows.Forms.Button();
+            this.LBLmoney = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(12, 13);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(331, 46);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "FRUIT MACHINE";
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(12, 227);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(480, 120);
+            this.btnRoll.TabIndex = 2;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = false;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // btnCashOut
+            // 
+            this.btnCashOut.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnCashOut.Enabled = false;
+            this.btnCashOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCashOut.Location = new System.Drawing.Point(12, 353);
+            this.btnCashOut.Name = "btnCashOut";
+            this.btnCashOut.Size = new System.Drawing.Size(480, 64);
+            this.btnCashOut.TabIndex = 6;
+            this.btnCashOut.Text = "Cash Out";
+            this.btnCashOut.UseVisualStyleBackColor = false;
+            this.btnCashOut.Click += new System.EventHandler(this.btnCashOut_Click);
+            // 
+            // LBLmoney
+            // 
+            this.LBLmoney.AutoSize = true;
+            this.LBLmoney.BackColor = System.Drawing.Color.RoyalBlue;
+            this.LBLmoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLmoney.Location = new System.Drawing.Point(373, 13);
+            this.LBLmoney.Name = "LBLmoney";
+            this.LBLmoney.Size = new System.Drawing.Size(97, 46);
+            this.LBLmoney.TabIndex = 7;
+            this.LBLmoney.Text = "1.00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(338, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 46);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "£";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(336, 62);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(156, 159);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 159);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(174, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(156, 159);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(503, 428);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LBLmoney);
+            this.Controls.Add(this.btnCashOut);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "Fruit Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnCashOut;
+        private System.Windows.Forms.Label LBLmoney;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FruitMachine/FruitMachine/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FruitMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Decimal Credit = 1;
+        List<string> Fruits = new List<string>() { "Cherry", "Bell","Lemon","Orange","Star","Skull"};
+        List<Image> FruitImages = new List<Image>() {Properties.Resources.Cherry, Properties.Resources.Bell, Properties.Resources.Lemon, Properties.Resources.Orange, Properties.Resources.Star, Properties.Resources.Skull };
+        List<string> UserFruits = new List<string>() { "","",""};
+        Random rnd = new Random();
+
+        private void Start() 
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                int randomchoice = rnd.Next(0, 6);
+                UserFruits[x] = Fruits[randomchoice];
+                ((PictureBox)this.Controls["pictureBox" + (x+1).ToString()]).BackgroundImage = FruitImages[randomchoice];
+            }
+            Credit = decimal.Subtract(Credit, Convert.ToDecimal(0.2));
+            CheckRoll();
+        }
+        private void CheckRoll() 
+        {
+            btnCashOut.Enabled = true;
+            if(UserFruits[0] == UserFruits[1] && UserFruits[0]== UserFruits[2]) 
+            {
+                if (UserFruits[0] == "Bell") 
+                {
+                    Credit = decimal.Add(Credit, Convert.ToDecimal(5));
+                }else if(UserFruits[0] == "Skull") 
+                {
+                    EndGame("You are broke lol");
+                }
+                else
+                {
+                    Credit = decimal.Add(Credit, Convert.ToDecimal(1));
+                }       
+            }else if (UserFruits[0]== UserFruits[1] || UserFruits[0] == UserFruits[2])
+            {
+                if(UserFruits[0] == "Skull") 
+                {
+                    Credit = decimal.Subtract(Credit, Convert.ToDecimal(1));
+                }
+                else 
+                {
+                    Credit = decimal.Add(Credit, Convert.ToDecimal(0.5));
+                }
+            }else if (UserFruits[1] == UserFruits[2]) 
+            {
+                if (UserFruits[1] == "Skull")
+                {
+                    Credit = decimal.Subtract(Credit, Convert.ToDecimal(1));
+                }
+                else
+                {
+                    Credit = decimal.Add(Credit, Convert.ToDecimal(0.5));
+                }
+            }
+            LBLmoney.Text = Credit.ToString() + "0";
+            if (Credit <= 0) 
+            {
+                EndGame("You are broke lol");
+            }
+        }
+        private void EndGame(string message) 
+        {
+            MessageBox.Show(message);
+            Environment.Exit(0);
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void btnCashOut_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Well Done , You won £"+Credit.ToString()+"0");
+            Environment.Exit(0);
+        }
+    }
+}
